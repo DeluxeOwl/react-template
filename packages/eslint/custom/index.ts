b1841b822dd7e3ed81cccd21dc821e10c792f94d
Andrei Surugiu <54327698+DeluxeOwl@users.noreply.github.com>
2026-03-04 11:06:11 +0200

    chore: remove meta

--- packages/eslint/custom/index.ts
+++ packages/eslint/custom/index.ts
@@ -4,10 +4,6 @@ import { preferDiscriminatedUnionRule } from "./discriminated-union"
 import { enforceNamespaceImportRule } from "./enforce-namespace-import"
 
 export default {
-    meta: {
-        name:    "eslint-plugin-sweepit",
-        version: "0.0.0",
-    },
     rules: {
         "enforce-namespace-import":   enforceNamespaceImportRule,
         "no-margin-on-root-jsx":      noMarginOnRootJSXRule,
